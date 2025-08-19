00611a7153c0b338d9d21186bfa5fea5fa845aa7
simoneee9 <hacajsimon@gmail.com>
2025-08-19 22:56:33 +0200

    get both physical and logical size for files

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ fast disk usage analyzer
 ## roadmap
 
 - [x] scan the current workdir and list the files present
-- [ ] get each file's logical & physical size
+- [x] get each file's logical & physical size
 - [ ] print a pretty, formatted output
 - [ ] show the size in appropriate units of magnitude (GiB, MiB, B) 
 - [ ] add cli args for the target directory and human/machine readable sizes
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
-use std::{env, ffi::OsStr, io::Error, path::{Path, PathBuf}};
+use std::{env, ffi::OsStr, fs::DirEntry, io::Error, os::unix::fs::MetadataExt, path::Path};
 
-fn print_osstring(osstr: &OsStr) {
-    if let Some(as_str) = osstr.to_str() {
-        println!("{as_str}");
-    } else {
-        eprintln!("WARNING: invalid utf8 sequence in path, skipping entry...");
+fn size_as_str(size: Option<u64>) -> String {
+    match size {
+        Some(size) => size.to_string(),
+        None => String::from("?"),
     }
 }
 
+fn print_entry(entry: &DirEntry, lsize: Option<u64>, psize: Option<u64>) -> Result<(), Error> {
+    let filename = entry.file_name();
+
+    let mut entry_str = String::from(filename.to_str().unwrap_or("invalid utf8 sequence"));
+
+    entry_str.push_str(":\tlog ");
+    entry_str.push_str(&size_as_str(lsize));
+
+    entry_str.push_str("\tphy ");
+    entry_str.push_str(&size_as_str(psize));
+
+    println!("{entry_str}");
+
+    Ok(())
+}
+
 fn traverse_dir(dir: &Path) -> Result<(), Error> {
-    for entry in dir.read_dir()? {
-        match entry {
-            Ok(entry) => {
-                print_osstring(&entry.file_name());
+    for entry in (dir.read_dir()?).flatten() {
+        let mut lsize: Option<u64> = None;
+        let mut psize: Option<u64> = None;
+        if let Ok(metadata) = entry.metadata() {
+            if !metadata.is_dir() {
+                lsize = Some(metadata.len());
+                psize = Some(metadata.blocks() * 512);
             }
-            Err(e) => eprintln!("{e}"),
+        }
+
+        if let Err(e) = print_entry(&entry, lsize, psize) {
+            eprintln!("WARNING: {e}. skipping entry...");
         }
     }
 
